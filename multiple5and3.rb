70122c9c4aaff2f4df9ed074c6ec286a5c2924a1
Marion Schleifer <marion.schleifer@simplificator.com>
2018-04-06 00:17:42 +0200

    multiplesof3and5

--- multiple5and3.rb
+++ multiple5and3.rb
@@ -1,2 +1,14 @@
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
 def multiple5and3
+  sum = 0
+
+  (0...1000).each do |n|
+    if n % 3 == 0 || n % 5 == 0
+      sum += n
+    end
+  end
+  puts sum
 end
+
+multiple5and3
